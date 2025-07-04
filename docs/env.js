5306f73a30e9b0dfc9707f4a9969c5db500f7b85
Neurona 1.0 <neuronapost@gmail.com>
2025-07-04 13:24:18 +0300

    env.js

---
+++ docs/env.js
@@ -0,0 +1,8 @@
+// docs/env.js
+window._env_ = {
+  OPENAI_API_KEY : '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',
+  CMC_API_KEY    : 'df5cedf1-5fa4-4641-b0b4-1fcc20f6925b',
+  COINAPI_KEY    : 'aade6fac0aaf9ab8c5d28e5d7760cb1a8d98beeb6fc99fd2a4b84ec6c4f83a55',
+  GNEWS_API_KEY  : '98bc30eaa117747c8b5b8de68d8ae960',
+  NEWSAPI_KEY    : 'ecc4cccff281465da6a16b244953a37c'
+};
