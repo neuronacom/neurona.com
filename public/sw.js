8485ddbc33a38190b1f34117c06088b45a754718
Neurona 1.0 <neuronapost@gmail.com>
2025-07-08 15:53:44 +0300

    sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,18 @@
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('push', function(event) {
+  let data = {};
+  if (event.data) data = event.data.json();
+  event.waitUntil(
+    self.registration.showNotification(data.title || 'NEURONA', {
+      body: data.body || '',
+      icon: '/27.png',
+      data: { url: data.url || '/' }
+    })
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  if (event.notification.data && event.notification.data.url) {
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+  }
+});
